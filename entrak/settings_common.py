e4d97ccfea799ad5a9ce1371406e4c0e128d72e6
tak <tak@en-trak.com>
2014-08-21 19:23:59 +0800

    heck to allow user edit in admin page

--- entrak/settings_common.py
+++ entrak/settings_common.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = (
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'django.contrib.humanize',
-	'mongoengine.django.mongo_auth',
 	'egauge',
 	'system',
 	'user',
--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import forms
 from .models import EntrakUser
+
+class EntrakUserCreationForm(UserCreationForm):
+	def clean_username(self):
+		# Since User.username is unique, this check is redundant,
+		# but it sets a nicer error message than the ORM. See #13147.
+		username = self.cleaned_data["username"]
+		try:
+			get_user_model()._default_manager.get(username=username)
+		except get_user_model().DoesNotExist:
+			return username
+		raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+	class Meta(UserCreationForm.Meta):
+		model = EntrakUser
 
 class EntrakUserAdmin(UserAdmin):
 	UserAdmin.list_display += ('system',)
+	list_editable = ('system',)
+
+	add_form = EntrakUserCreationForm
 
 admin.site.register(EntrakUser, EntrakUserAdmin)
