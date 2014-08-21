e241119101ed6ade80fb586ba44ce0fbc3ed278a
tak <tak@en-trak.com>
2014-08-21 22:14:50 +0800

    make backup form v2 csv as python mangement func

--- entrak/management/commands/backup_from_v2_csv.py
+++ entrak/management/commands/backup_from_v2_csv.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
-
 import os
 import sys
 import pymongo
 import csv
 import datetime
 import pytz
+from django.core.management.base import BaseCommand, CommandError
+from mongoengine import connection
 
 SOURCE_MAPPING = {
 	# 'school1': [
@@ -78,36 +78,40 @@ DATA_MAPPING = {
 	'year': {'collection': 'source_reading_year', 'verify_func': is_year_datetime_valid},
 }
 
-client = pymongo.MongoClient()
-hk_tz = pytz.timezone('Asia/Hong_Kong')
-target_dir = sys.argv[1]
-file_loop_generator = (dir_name for dir_name in os.listdir(target_dir) if os.path.isdir(os.path.join(target_dir, dir_name)))
-for dir_name in file_loop_generator:
-	print 'processing dir: ', dir_name
-	source_ids = []
-	if dir_name in SOURCE_MAPPING:
-		for source_info in SOURCE_MAPPING[dir_name]:
-			xml_url = source_info['xml_url']
-			name = source_info['name']
-			source_id = client.entrak.source.find_one({'xml_url': xml_url, 'name': name})['_id']
-			source_ids.append(source_id)
+class Command(BaseCommand):
 
-	dir_path = os.path.join(target_dir, dir_name)
-	for data_type in ['min', 'hour', 'day', 'week', 'month', 'year']:
-		print 'processing data_type: ', data_type
-		with open(os.path.join(dir_path, (data_type+'.txt')), 'rb') as f:
-			reader = csv.reader(f)
-			for row in reader:
-				datetime_str = row[0]
-				datetime_obj = hk_tz.localize(datetime.datetime.strptime(datetime_str ,"%Y-%m-%d %H:%M:%S"))
-				if DATA_MAPPING[data_type]['verify_func'](datetime_obj):
-					source_values = row[1:3]
-					try:
-						client.entrak[DATA_MAPPING[data_type]['collection']].insert([{
-							'source_id': source_id,
-							'datetime': datetime_obj,
-							'value': float(source_values[idx])
-						} for (idx, source_id) in enumerate(source_ids)], continue_on_error=True)
-					except pymongo.errors.DuplicateKeyError, e:
-						# do nothing
-						pass
+	def handle(self, *args, **options):
+		client = connection.get_db()
+
+		hk_tz = pytz.timezone('Asia/Hong_Kong')
+		target_dir = args[0]
+		file_loop_generator = (dir_name for dir_name in os.listdir(target_dir) if os.path.isdir(os.path.join(target_dir, dir_name)))
+		for dir_name in file_loop_generator:
+			self.stdout.write('processing dir: '+dir_name)
+			source_ids = []
+			if dir_name in SOURCE_MAPPING:
+				for source_info in SOURCE_MAPPING[dir_name]:
+					xml_url = source_info['xml_url']
+					name = source_info['name']
+					source_id = client.entrak.source.find_one({'xml_url': xml_url, 'name': name})['_id']
+					source_ids.append(source_id)
+
+			dir_path = os.path.join(target_dir, dir_name)
+			for data_type in ['min', 'hour', 'day', 'week', 'month', 'year']:
+				self.stdout.write('processing data_type: '+data_type)
+				with open(os.path.join(dir_path, (data_type+'.txt')), 'rb') as f:
+					reader = csv.reader(f)
+					for row in reader:
+						datetime_str = row[0]
+						datetime_obj = hk_tz.localize(datetime.datetime.strptime(datetime_str ,"%Y-%m-%d %H:%M:%S"))
+						if DATA_MAPPING[data_type]['verify_func'](datetime_obj):
+							source_values = row[1:3]
+							try:
+								client.entrak[DATA_MAPPING[data_type]['collection']].insert([{
+									'source_id': source_id,
+									'datetime': datetime_obj,
+									'value': float(source_values[idx])
+								} for (idx, source_id) in enumerate(source_ids)], continue_on_error=True)
+							except pymongo.errors.DuplicateKeyError, e:
+								# do nothing
+								pass
