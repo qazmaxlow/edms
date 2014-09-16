e2d2dd872c6f3ed965021a7d5e815d02a8294374
tak <tak@en-trak.com>
2014-09-16 15:04:54 +0800

    alert history skeleton and fix animation on safari

--- entrak/settings_views.py
+++ entrak/settings_views.py
@@ -1,30 +1,64 @@
 import datetime
 import json
+import pytz
+import calendar
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.html import escapejs
+from django.db.models import Q
 from system.models import System
 from egauge.manager import SourceManager
-from alert.models import Alert, ALERT_TYPE_STILL_ON, ALERT_TYPE_SUMMARY, ALERT_TYPE_PEAK, ALERT_COMPARE_METHOD_ABOVE
+from alert.models import Alert, AlertHistory, ALERT_TYPE_STILL_ON, ALERT_TYPE_SUMMARY, ALERT_TYPE_PEAK, ALERT_COMPARE_METHOD_ABOVE
 from contact.models import Contact
 from user.models import USER_ROLE_ADMIN_LEVEL
 from utils.auth import permission_required
 from utils.utils import Utils
 
-SETTINGS_TYPE_ALERT = 'alert'
+SHOW_HISTORY_DAY_MAX = 7
 
 @permission_required(USER_ROLE_ADMIN_LEVEL)
 @ensure_csrf_cookie
 def alert_settings_view(request, system_code=None):
 	systems_info = System.get_systems_info(system_code, request.user.system.code)
-	sources = SourceManager.get_sources(systems_info["systems"][0])
+	current_system = systems_info["systems"][0]
+	sources = SourceManager.get_sources(current_system)
 	contact_emails = Contact.objects.filter(system__code=system_code).values_list('email', flat=True)
 	alerts = Alert.objects.filter(system_id__in=[system.id for system in systems_info["systems"]]).prefetch_related('contacts')
+
+	system_id = current_system.id
+	system_timezone = pytz.timezone(current_system.timezone)
+
+	bound_dt = pytz.utc.localize(datetime.datetime.utcnow()) - datetime.timedelta(days=SHOW_HISTORY_DAY_MAX)
+	alert_historys = AlertHistory.objects.select_related('alert').filter(
+		Q(created__gte=bound_dt) | Q(resolved_datetime__gte=bound_dt),
+		alert__system_id=system_id).order_by('-created')
+
+	alert_history_infos = []
+	for alert_history in alert_historys:
+		info = {
+			'created': calendar.timegm(alert_history.created.utctimetuple()),
+			'name': alert_history.alert.source_info['name'],
+			'alert_type': alert_history.alert.type,
+			'diff_percent': alert_history.diff_percent,
+			'check_weekdays': alert_history.alert.check_weekdays,
+			'resolved': alert_history.resolved,
+		}
+		if alert_history.resolved:
+			info['resolved_datetime'] = calendar.timegm(alert_history.resolved_datetime.utctimetuple())
+		if alert_history.alert.type == ALERT_TYPE_SUMMARY:
+			info['start_time_h'] = alert_history.alert.start_time.hour
+			info['start_time_m'] = alert_history.alert.start_time.minute
+			info['end_time_h'] = alert_history.alert.end_time.hour
+			info['end_time_m'] = alert_history.alert.end_time.minute
+
+		alert_history_infos.append(info)
 
 	m = systems_info
 	m['sources'] = sources
 	m['contact_emails'] = [email.encode('utf8') for email in contact_emails]
 	m['alerts'] = alerts
+	m['alert_history_infos'] = escapejs(json.dumps(alert_history_infos))
 	m.update(csrf(request))
 
 	return render_to_response('alert_settings.html', m)
