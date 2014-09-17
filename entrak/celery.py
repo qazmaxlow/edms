95f14f55aeff517023866ebc31c6daf48679c9eb
tak <tak@en-trak.com>
2014-09-17 18:28:51 +0800

    add check alert tasks

--- entrak/celery.py
+++ entrak/celery.py
@@ -9,28 +9,36 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entrak.settings')
 
 app = Celery('entrak',
-             broker='amqp://localhost')
+			 broker='amqp://localhost')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # Optional configuration, see the application user guide.
 app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=10,
-    CELERY_DISABLE_RATE_LIMITS=True,
-    CELERY_IGNORE_RESULT=True,
+	CELERY_TASK_RESULT_EXPIRES=10,
+	CELERY_DISABLE_RATE_LIMITS=True,
+	CELERY_IGNORE_RESULT=True,
 
 	CELERYBEAT_SCHEDULE = {
 		'retrieve-reading-every-min': {
 			'task': 'egauge.tasks.retrieve_all_reading',
 			'schedule': crontab(),
-    	},
-    	'recover-reading-every-hour': {
+		},
+		'recover-reading-every-hour': {
 			'task': 'egauge.tasks.recover_all_invalid_reading',
 			'schedule': crontab(minute=0, hour='*'),
-    	},
+		},
+		'check-alert-every-5-min': {
+			'task': 'alert.tasks.check_all_alerts',
+			'schedule': crontab(minute='*/5'),
+		},
+		'send-alert-email-every-5-min': {
+			'task': 'alert.tasks.send_alert_email',
+			'schedule': crontab(minute='*/5'),
+		},
 	}
 )
 
 if __name__ == '__main__':
-    app.start()
+	app.start()
