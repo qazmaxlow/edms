3b01f057be8dda41316aa9e7ccb2033f0c7a4dcd
tak <tak@en-trak.com>
2014-10-13 17:24:03 +0800

    add some filter to alert admin

--- alert/admin.py
+++ alert/admin.py
@@ -4,6 +4,8 @@ from .models import Alert, AlertHistory, AlertEmail
 class AlertAdmin(admin.ModelAdmin):
     list_display = ('system', 'type', 'check_weekdays', 'compare_percent',
         'peak_threshold', 'start_time', 'end_time', 'created')
+    list_filter = ('system', 'type')
+    search_fields = ['system__code']
 
 class AlertHistoryAdmin(admin.ModelAdmin):
     list_display = ('alert', 'created', 'diff_percent', 'resolved', 'resolved_datetime')
