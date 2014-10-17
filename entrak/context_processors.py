653b99d548ffd06d790f7a73caffedc60aa236a1
tak <tak@en-trak.com>
2014-10-17 15:22:36 +0800

    assign page title and system code for google analytics

--- entrak/context_processors.py
+++ entrak/context_processors.py
@@ -1,5 +1,7 @@
 from settings import ANALYTICS_TRACKING
+from django.core.urlresolvers import resolve
 
 def analytics(request):
-    context_extras = {'ANALYTICS_TRACKING': ANALYTICS_TRACKING}
+    system_code = resolve(request.path).kwargs.get('system_code', '')
+    context_extras = {'ANALYTICS_TRACKING': ANALYTICS_TRACKING, 'SYSTEM_CODE': system_code}
     return context_extras
