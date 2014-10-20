cd0a5ffc22afcc799fc7b809e31fc9945f5293b2
noegry <noegry0@gmail.com>
2014-10-20 17:01:51 +0800

    Change p_id to code

--- printers/admin.py
+++ printers/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Printer
 
 class PrinterAdmin(admin.ModelAdmin):
-    list_display = ('p_id', 'name', 'system', 'order')
+    list_display = ('code', 'name', 'system', 'order')
 
 admin.site.register(Printer, PrinterAdmin)
