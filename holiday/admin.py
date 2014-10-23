4ae35783b1bf4909d498457b86d3f16b92d62ed2
tak <tak@en-trak.com>
2014-10-23 14:57:25 +0800

    enhance add holiday to apply to sub-systems

--- holiday/admin.py
+++ holiday/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CityHoliday, Holiday
+from system.models import System
 
 class CityHolidayAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'desc', 'city')
@@ -12,6 +13,20 @@ class HolidayAdmin(admin.ModelAdmin):
     list_editable = ('system', 'date', 'desc')
     list_filter = ('system', )
     ordering = ('system', 'date')
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.save()
+        else:
+            if request.POST.get('apply_subsystem') == 'on':
+                systems = System.get_systems_within_root(obj.system.code)
+                Holiday.objects.bulk_create([Holiday(
+                    system_id=system.id,
+                    date=obj.date,
+                    desc=obj.desc
+                ) for system in systems])
+            else:
+                obj.save()
 
 admin.site.register(CityHoliday, CityHolidayAdmin)
 admin.site.register(Holiday, HolidayAdmin)
