1394db74f3ec265e110036adaff5b2b2c3e75423
tak <tak@en-trak.com>
2014-10-23 10:15:45 +0800

    enhance holiday and baseline admin

--- holiday/admin.py
+++ holiday/admin.py
@@ -5,11 +5,13 @@ class CityHolidayAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'desc', 'city')
     list_editable = ('date', 'desc', 'city')
     list_filter = ('city', )
+    ordering = ('city', 'date')
 
 class HolidayAdmin(admin.ModelAdmin):
     list_display = ('id', 'system', 'date', 'desc')
     list_editable = ('system', 'date', 'desc')
     list_filter = ('system', )
+    ordering = ('system', 'date')
 
 admin.site.register(CityHoliday, CityHolidayAdmin)
 admin.site.register(Holiday, HolidayAdmin)
