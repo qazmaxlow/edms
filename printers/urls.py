0543d62f6d0f4a0d0637291212c45a4146e041c1
noegry <noegry0@gmail.com>
2014-10-30 18:15:41 +0800

    Add printer graph url name

--- printers/urls.py
+++ printers/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 from . import graph_views
 
 urlpatterns = patterns('',
-    url(r'^graph/$', graph_views.graph_view),
+    url(r'^graph/$', graph_views.graph_view,
+        name="printers.graph"),
     url(r'^measures/show/$', graph_views.show_measures_view),
     url(r'^measures/show_highest_and_lowest/$', graph_views.show_highest_and_lowest_view),
     url(r'^measures/show_summary/$', graph_views.show_summary_view),
