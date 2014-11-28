1c4b2755044df8fcc1a7a0fa3efc7b4cd772a59c
Eric Cheung <eric.cheung@en-trak.com>
2014-11-28 16:35:54 +0800

    Add settings file for the wsgi docker

---
+++ dockers/wsgi/settings.py
@@ -0,0 +1,22 @@
+import os
+from mongoengine import connect
+from settings_common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'entrak',
+        'USER': 'root',
+        'PASSWORD': '99924361',
+    }
+}
+
+connect('entrak', host='mongo') #for docker
+
+MEDIA_URL = '/media/'
+LOGGING['handlers']['timed_rotating_handler']['filename'] = os.path.join(os.path.dirname(BASE_DIR), 'logs', 'entrak', 'error_log')
+LOGGING['handlers']['recap_timed_rotating_handler']['filename'] = os.path.join(os.path.dirname(BASE_DIR), 'logs', 'entrak', 'recap_log')
+
+SITE_LINK_FORMAT = 'http://192.168.1.103:8001'
+
+# PYTHON_BIN = '/app/ENV/entrak/bin/python'
