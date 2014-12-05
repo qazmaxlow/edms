92f1bc8d9440cbdf7ea4d2b1d4e2fff386d1d13f
Eric Cheung <eric.cheung@en-trak.com>
2014-12-05 16:27:42 +0800

    Fixed no data show in the exported data file

--- entrak/export_data_views.py
+++ entrak/export_data_views.py
@@ -22,8 +22,8 @@ class PseudoBuffer(object):
 def __result_generator(source_readings, source_id_map, unit_category_code, money_unit_rates, co2_unit_rates, system=None):
     # Generate CSV header
     sources = SourceManager.get_sources(system)
-    source_headers = [s.d_name for s in sources]
-    csv_header = ["Date Time"] + source_headers
+    source_headers = [s.name for s in sources]
+    csv_header = ["Date Time"] + [s.d_name for s in sources]
     yield csv_header
 
     last_ts = None
