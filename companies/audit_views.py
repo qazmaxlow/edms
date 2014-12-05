00d6ac7047bfa7e8fd8e29a7f8c9c56178d64831
Eric Cheung <eric.cheung@en-trak.com>
2014-12-05 10:11:03 +0800

    Show only the users in the system

--- companies/audit_views.py
+++ companies/audit_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
@@ -11,6 +12,15 @@ class AuditTrailFilter(django_filters.FilterSet):
         model = Trail
         fields = ['user',]
 
+    def __init__(self, company_syscode, *args, **kwargs):
+        super(AuditTrailFilter, self).__init__(*args, **kwargs)
+        User = get_user_model()
+        user_queryset = User.objects.filter(system__code=company_syscode)
+        self.filters['user'].extra.update(
+            {'empty_label': 'All user',
+             'queryset': user_queryset,
+         })
+
 
 class CompanyAuditTrailsListView(ListView):
     template_name = 'companies/audit/trails/list.html'
@@ -19,7 +29,7 @@ class CompanyAuditTrailsListView(ListView):
     def get_queryset(self):
         company_syscode = self.kwargs['system_code']
         trail_queryset = Trail.objects.filter(user__system__code=company_syscode)
-        self.filter = AuditTrailFilter(self.request.GET, queryset=trail_queryset)
+        self.filter = AuditTrailFilter(company_syscode, self.request.GET, queryset=trail_queryset)
         return self.filter.qs
 
     def get_context_data(self, **kwargs):
