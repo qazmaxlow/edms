45aa418eb04786fef58d0b57d11b02ed8ed13b2e
Eric Cheung <eric.cheung@en-trak.com>
2014-12-05 09:48:26 +0800

    Show only the trails which users are in the system code

--- companies/audit_views.py
+++ companies/audit_views.py
@@ -17,7 +17,9 @@ class CompanyAuditTrailsListView(ListView):
     paginate_by = 30
 
     def get_queryset(self):
-        self.filter = AuditTrailFilter(self.request.GET, queryset=Trail.objects.all())
+        company_syscode = self.kwargs['system_code']
+        trail_queryset = Trail.objects.filter(user__system__code=company_syscode)
+        self.filter = AuditTrailFilter(self.request.GET, queryset=trail_queryset)
         return self.filter.qs
 
     def get_context_data(self, **kwargs):
