df7d69706962e1b5ef8d05df398b6497c8b358c2
Eric Cheung <eric.cheung@en-trak.com>
2014-12-08 12:53:31 +0800

    Log only when it is Entrak user

--- audit/decorators/trail.py
+++ audit/decorators/trail.py
@@ -1,9 +1,12 @@
 from functools import wraps
 
+from django.contrib.auth import get_user_model
 from django.utils.decorators import available_attrs
 
 from audit.models import Trail
 
+
+User = get_user_model()
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -21,12 +24,13 @@ def log_audit_trail(action_type):
     def decorator(func):
         @wraps(func, assigned=available_attrs(func))
         def inner(request, *args, **kwargs):
-            Trail.objects.create(action_type=action_type,
-                          user=request.user,
-                          session_key=request.session.session_key,
-                          ip_address=get_client_ip(request),
-                          url=request.build_absolute_uri()
-            )
+            if isinstance(request.user, User):
+                Trail.objects.create(action_type=action_type,
+                                     user=request.user,
+                                     session_key=request.session.session_key,
+                                     ip_address=get_client_ip(request),
+                                     url=request.build_absolute_uri()
+                )
 
             return func(request, *args, **kwargs)
         return inner
