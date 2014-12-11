22f96b273079cc62394d5a1a04e42aa1ee299b63
Eric Cheung <eric.cheung@en-trak.com>
2014-12-11 14:50:53 +0800

    Set entrakuser nested in trail

--- companies/serializers.py
+++ companies/serializers.py
@@ -3,7 +3,18 @@ from rest_framework import serializers
 from audit.models import Trail
 
 
-class TrailSerializer(serializers.ModelSerializer):
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class EntrakuserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('fullname', 'username')
+
+
+class TrailSerializer(serializers.HyperlinkedModelSerializer):
+    user = EntrakuserSerializer()
     class Meta:
         model = Trail
         fields = ('action_type', 'user', 'created_time')
