dff17563e4981b3d0a9f30cb3e1d03a4c6a50090
Eric Cheung <eric.cheung@en-trak.com>
2014-12-12 13:16:26 +0800

    Add date filters

--- companies/apis.py
+++ companies/apis.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from audit.models import Trail
+from .audit_views import AuditTrailFilter
 from .serializers import TrailSerializer
 
 
@@ -8,10 +9,5 @@ class TrailViewSet(viewsets.ModelViewSet):
     queryset = Trail.objects.all()
     serializer_class = TrailSerializer
     paginate_by = 30
-
-    def get_queryset(self):
-        queryset = Trail.objects.all()
-        user = self.request.QUERY_PARAMS.get('user', None)
-        if user:
-            queryset = queryset.filter(user=user)
-        return queryset
+    filter_class = AuditTrailFilter
+    filter_backends = (filters.DjangoFilterBackend,)
