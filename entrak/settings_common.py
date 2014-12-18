b47c671667a2d159a5dc120041dae93a2b2b875d
Eric Cheung <eric.cheung@en-trak.com>
2014-12-18 14:36:30 +0800

    Set new static root path

--- entrak/settings_common.py
+++ entrak/settings_common.py
@@ -15,6 +15,8 @@ from django.utils.translation import ugettext_lazy as _
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = lambda *a: os.path.join(ROOT, *a)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -29,7 +31,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'entrak', 'static')
+STATIC_ROOT = path('site-static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'django.contrib.staticfiles.finders.FileSystemFinder',
