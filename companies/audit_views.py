619ce1077fc1352596de5a8eeb7bf2e64da256df
tak <tak@en-trak.com>
2014-12-29 17:15:59 +0800

    Add staff click count button

--- companies/audit_views.py
+++ companies/audit_views.py
@@ -10,6 +10,7 @@ from django.utils.decorators import method_decorator
 from django.utils.timezone import localtime
 
 import django_filters
+from trail_table import *
 
 from audit.models import Trail
 from system.models import System
@@ -26,7 +27,6 @@ class AuditTrailFilter(django_filters.FilterSet):
 class ExportCsvMixin(object):
     csv_limited_record = 10000
     csv_download_button = 'download_csv'
-
     def get_csv_filename(self):
         if self.csv_filename is None:
             raise Exception("ExportCsvMixin requires a definition of csv_filename")
@@ -62,10 +62,73 @@ class ExportCsvMixin(object):
             response.write(csv_io.getvalue())
             return response
         else:
-            return super(ExportCsvMixin, self).get(request, *args, **kwargs)
+            return super(ExportCsvMixin, self).post(request, *args, **kwargs)
+
+class ExportCsvMixin2(object):
+    csv_limited_record2 = 10000
+    csv_download_button2 = 'download_csv_click_count'
+    def get_csv_filename(self):
+        if self.csv_filename is None:
+            raise Exception("ExportCsvMixin requires a definition of csv_filename")
+        else:
+            return self.csv_filename
+
+    def post(self, request, *args, **kwargs):
+        if self.csv_download_button2 in self.request.POST:
+            objects = self.get_queryset()
+
+            if self.csv_limited_record2:
+                objects = objects[:self.csv_limited_record2]
+
+            csv_io = StringIO.StringIO()
+            csv_wr = csv.writer(csv_io)
+
+            def get_csv_val(o, f):
+                v = o
+                for subf in f.split('.'):
+                    v = getattr(v, subf)
+
+                if isinstance(v, datetime.date):
+                    v = localtime(v)
+
+                return v
+
+            c=Company()
+            for obj in objects:
+                csv_vals = map(lambda f: get_csv_val(obj, f), self.csv_fields)
+                append = csv_vals_Append_Format(csv_vals)
+                c.csv_append([append.acc_name,append.full_name,append.action,append.trail_hour,append.trail_date])
+            for date in c.date:
+                date_in_row=[]
+                date_in_row.append(date)
+                csv_wr.writerow(date_in_row)
+                time=[]
+                time.append("Time:")
+                time_range=range(22,3,-1)
+                for t in time_range:
+                    time.append("%d:00"%t)
+                csv_wr.writerow(time)
+
+                for user in c.user:
+                    user_trail=[]
+                    user_trail.append(user)
+                    for t in time_range:
+                        user_trail.append(c.total_action(user,date,t))
+                    csv_wr.writerow(user_trail)
+                linespacing=[]
+                csv_wr.writerow(linespacing)
 
 
-class CompanyAuditTrailsListView(ExportCsvMixin, ListView):
+
+            response = http.HttpResponse(mimetype='text/csv')
+            filename = self.get_csv_filename()
+            response['Content-Disposition'] = 'attachment; filename="{0}"'.format(filename)
+            response.write(csv_io.getvalue())
+            return response
+        else:
+            return super(ExportCsvMixin2, self).get(request, *args, **kwargs)
+
+class CompanyAuditTrailsListView(ExportCsvMixin, ExportCsvMixin2, ListView):
     csv_filename = 'audit_trails.csv'
     csv_fields = ['user', 'user.fullname', 'action_name', 'created_time']
     template_name = 'companies/audit/trails/list.html'
@@ -78,7 +141,7 @@ class CompanyAuditTrailsListView(ExportCsvMixin, ListView):
     def get_queryset(self):
         company_syscode = self.kwargs['system_code']
         trail_queryset = Trail.objects.filter(user__system__code=company_syscode)
-        self.filter = AuditTrailFilter(self.request.GET, queryset=trail_queryset)
+        self.filter = AuditTrailFilter(self.request.POST, queryset=trail_queryset)
         return self.filter.qs
 
     def get_context_data(self, **kwargs):
---
+++ companies/trail_table.py
@@ -0,0 +1,83 @@
+class Company:
+    def __init__(self):
+        self.date=[]
+        self.user=[]
+        self.User_list=[]
+        self.csv=[]
+    def total_action(self,user,day,timeslot):
+        if user in self.user:
+            user_index=self.user.index(user)
+            return self.User_list[user_index].total_action(day,timeslot)
+        else:
+            return 0
+    def add_user(self,user):
+        if user in self.user:#check duplicate
+            return
+        else:
+            self.user.append(user)
+            self.User_list.append(User(user))
+    def csv_append(self,record):
+        self.csv.append(record)
+        user=record[1]
+        action=record[2]
+        timeslot=record[3]
+        date=record[4]
+        self.add_day(date)
+        self.add_user(user)
+        user_index=self.user.index(user)
+        self.User_list[user_index].add_action(action,timeslot,date)
+    def add_day(self,date):
+        if date not in self.date:
+            self.date.append(date)
+
+class User:
+    def __init__(self,username):
+        self.name=username
+        self.trail_day=[]
+        self.Trail_day=[]
+    def add_day(self,day):
+        if day in self.trail_day:
+            return
+        else:
+            self.trail_day.append(day)
+            self.Trail_day.append(Day(day))
+    def add_action(self,action,timeslot,date):
+        self.add_day(date)
+        day_index=self.trail_day.index(date)
+        self.Trail_day[day_index].add_action(action,timeslot,date)
+    def total_action(self,day,timeslot):
+        if day not in self.trail_day:
+            return 0
+        day_index=self.trail_day.index(day)
+        return self.Trail_day[day_index].total_action(timeslot)
+class Day:
+    def __init__(self,day):
+        self.day=day
+        self.timeslot=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+        self.Timeslot_list=[]
+        for time in self.timeslot:
+            self.Timeslot_list.append(Timeslot(time))
+    def add_action(self,action,timeslot,date):
+        time_index=self.timeslot.index(timeslot)
+        if action in self.Timeslot_list[time_index].action:
+            return
+        else:
+            self.Timeslot_list[time_index].action.append(action)
+    def total_action(self,timeslot):
+        time_index=self.timeslot.index(timeslot)
+        return self.Timeslot_list[time_index].total_action()
+class Timeslot:
+    def __init__(self,timeslot):
+        self.timeslot=timeslot
+        self.action=[]
+    def total_action(self):
+        return len(self.action)
+class csv_vals_Append_Format:
+    def __init__(self,csv_vals):
+        self.trail_date = csv_vals[3].date()
+        self.trail_hour = csv_vals[3].time().hour
+        self.action = csv_vals[2]
+        self.full_name = csv_vals[1]
+        if (csv_vals[1] is None):
+            self.full_name = csv_vals[0]
+        self.acc_name=csv_vals[0]
