b72a686573bc036dbd345f2285b1477ea3351587
Eric Cheung <eric.cheung@en-trak.com>
2015-01-14 17:44:41 +0800

    Add cars

--- companies/report_views.py
+++ companies/report_views.py
@@ -193,6 +193,9 @@ def popup_report_view(request, system_code, year, month):
     m['saving_co2'] = -1 * report_data['savingInfo']['co2'] / 1000.0
     m['saving_money'] = -1 * report_data['savingInfo']['money']
 
+    m['co2_in_car'] = abs(report_data['savingInfo']['co2']*0.003)
+    # var co2InCar = Utils.formatWithCommas(Math.abs((reportGenThis.savingInfo.co2*0.003).toFixed(0)));
+
     # lastSamePeriodUsage += info[lastSamePeriodUsageKey].average;
 
     # this.generateCalendarReport('#weekday-info', combinedReadings,
