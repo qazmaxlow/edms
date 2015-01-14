d02a0c56d282beaf802a895481e207e16ae0d59c
Eric Cheung <eric.cheung@en-trak.com>
2015-01-14 17:16:15 +0800

    Add saving money

--- companies/report_views.py
+++ companies/report_views.py
@@ -191,6 +191,7 @@ def popup_report_view(request, system_code, year, month):
     m['saving_energy'] = -1 * report_data['savingInfo']['energy']
     # in tons
     m['saving_co2'] = -1 * report_data['savingInfo']['co2'] / 1000.0
+    m['saving_money'] = -1 * report_data['savingInfo']['money']
 
     # lastSamePeriodUsage += info[lastSamePeriodUsageKey].average;
 
