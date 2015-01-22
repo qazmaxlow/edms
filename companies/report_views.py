4e0d3c95d4a99b81ed0e0047ec3c2c124e667945
Eric Cheung <eric.cheung@en-trak.com>
2015-01-22 13:36:47 +0800

    Add axia label

--- companies/report_views.py
+++ companies/report_views.py
@@ -363,6 +363,9 @@ def popup_report_view(request, system_code, year, month):
     m['compare_current_readings_series']= json.dumps(combined_readings.values(), cls=DjangoJSONEncoder)
     m['compare_current_readings_month'] = report_date.strftime('%b')
 
+    m['compare_last_readings_series']= json.dumps(combined_last_readings.values(), cls=DjangoJSONEncoder)
+
+    m['compare_last_readings_month'] = previous_month(report_date).strftime('%b')
     # assert False
 
 
