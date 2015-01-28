7aea47a20b3d07d39fa68117dfc19aff68d470ab
tak <tak@en-trak.com>
2015-01-28 18:14:53 +0800

    modify to be better code

--- trails/trails.py
+++ trails/trails.py
@@ -53,10 +53,8 @@ class ExportCsvMixin(object):
 
             for obj in objects:
                 csv_vals = map(lambda f: get_csv_val(obj, f), self.csv_fields)
-                if obj.user.system!=None:
-                    csv_vals.insert(0, obj.user.system.name)
-                else:
-                    csv_vals.insert(0,"")
+                system_name = obj.user.system.name if obj.user.system else None
+                csv_vals.insert(0, system_name)
                 csv_wr.writerow(csv_vals)
                 
                 
