a8568cac6b53529c775eca9bad79bf70a18c57ef
Eric Cheung <eric.cheung@en-trak.com>
2015-03-13 16:29:28 +0800

    Set humun format to total cost

--- companies/report_views.py
+++ companies/report_views.py
@@ -178,7 +178,7 @@ def summary_ajax(request, system_code):
     # m = systems_info
     m = {}
 
-    m['formated_total_cost'] = '${0:.0f}'.format(total_cost) if total_cost is not None else None
+    m['formated_total_cost'] = '${0:,.0f}'.format(total_cost) if total_cost is not None else None
     m['formated_weekday_cost'] = '${0:.0f}'.format(weekday_cost) if weekday_cost is not None else None
 
     m['compare_to_last_total'] = CompareTplHepler(compare_to_last_total).to_dict()
