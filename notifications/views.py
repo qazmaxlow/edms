87a928928e63083e642fe428182d8d61808e6073
Eric Cheung <eric.cheung@en-trak.com>
2015-03-24 16:46:51 +0800

    Add get notifications.messages view

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, mixins
+
+from .models import Message
+from .serializers import MessageSerializer
+
+
+class MessageList(generics.ListAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
