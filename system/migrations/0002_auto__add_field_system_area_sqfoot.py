d1c488d8ba0684eee1cde3167d12ff1962b42d26
Eric Cheung <eric.cheung@en-trak.com>
2015-04-09 10:56:06 +0800

    Add square foot in the system model

---
+++ system/migrations/0002_auto__add_field_system_area_sqfoot.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'System.area_sqfoot'
+        db.add_column(u'system_system', 'area_sqfoot',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'System.area_sqfoot'
+        db.delete_column(u'system_system', 'area_sqfoot')
+
+
+    models = {
+        u'system.system': {
+            'Meta': {'object_name': 'System'},
+            'area_sqfoot': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'default': "'hk'", 'max_length': '200'}),
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'first_record': ('django.db.models.fields.DateTimeField', [], {}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'full_name_tc': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'intro': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'intro_tc': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'last_update': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'null': 'True', 'blank': 'True'}),
+            'lat': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'lng': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'login_required': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'name_tc': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'night_time_end': ('django.db.models.fields.TimeField', [], {'default': 'datetime.time(7, 0)'}),
+            'night_time_start': ('django.db.models.fields.TimeField', [], {'default': 'datetime.time(22, 0)'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'population': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            'timezone': ('django.db.models.fields.CharField', [], {'default': "u'Asia/Hong_Kong'", 'max_length': '50'}),
+            'unit_info': ('django.db.models.fields.TextField', [], {'default': "'{}'"})
+        },
+        u'system.systemhomeimage': {
+            'Meta': {'object_name': 'SystemHomeImage'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'system': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['system.System']"})
+        }
+    }
+
+    complete_apps = ['system']
