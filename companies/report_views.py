fc901f47f6a9cf9224af93b8a391c45f61cf7768
Eric Cheung <eric.cheung@en-trak.com>
2015-04-10 14:33:43 +0800

    Add javascript delay

--- companies/report_views.py
+++ companies/report_views.py
@@ -1171,6 +1171,7 @@ def _popup_report_view(request, system_code, year=None, month=None, report_type=
             # show_content_in_browser=False,
             cmd_options={
                 'page-size': 'A3',
+                'javascript-delay': 1000,
                 # 'quiet': '',
                 # 'margin-left': '18mm',
                 # 'page-size': 'A3',
