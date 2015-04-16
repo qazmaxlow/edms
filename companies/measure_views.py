4c9f283cec256fcd201b46a8de25af4a2b25ee9c
Eric Cheung <eric.cheung@en-trak.com>
2015-04-16 10:54:31 +0800

    Group daily measure by datetime

--- companies/measure_views.py
+++ companies/measure_views.py
@@ -1,5 +1,6 @@
 import datetime, pytz
 
+from mongoengine import connection
 from rest_framework import generics, mixins
 
 from system.models import System
@@ -8,6 +9,7 @@ from .serializers import MeasureSerializer
 
 class DailyMeasureList(generics.ListAPIView):
     serializer_class = MeasureSerializer
+
     def get_queryset(self):
         syscode = self.kwargs['system_code']
         sys = System.objects.get(code=syscode)
@@ -17,10 +19,23 @@ class DailyMeasureList(generics.ListAPIView):
             hour=0, minute=0, second=0, microsecond=0)
         date_end = date_start + datetime.timedelta(days=1)
 
-        measured_entries = SourceReadingHour.objects(
-            source_id__in=source_ids,
-            datetime__gte=date_start,
-            datetime__lt=date_end
-        )
+        mdb_conn = connection.get_db()
+        measured_entries = mdb_conn.source_reading_hour.aggregate([
+            { "$match":
+              {
+                  "source_id": {"$in": source_ids},
+                  "datetime": {"$gte": date_start, "$lt": date_end }
+              }
+          },
+            { "$group":
+              {
+                  "_id": "$datetime",
+                  "value": {"$sum": "$value"}
+              }
+          }
+        ])
 
-        return measured_entries
+        results = measured_entries['result']
+        json_data = [{'datetime': m['_id'], 'value': m['value']} for m in results]
+
+        return json_data
