1e9c36ca098baff84798e851558b89641358cacd
Yeung Leung <hotdogxz@gmail.com>
2015-04-27 18:47:09 +0800

    Added user activation URL's generation and validation

--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from Crypto.Cipher import AES
 from datetime import datetime
+from entrak.encrypter import EntrakEncrypter
 
 
 USER_ROLE_ADMIN_LEVEL   = 100
@@ -12,8 +13,8 @@ USER_ROLE_CHOICES = (
     (USER_ROLE_VIEWER_LEVEL, 'viewer'),
 )
 
-ENGLISH     = 1
-CHINESE     = 2
+ENGLISH     = "en_US"
+CHINESE     = "zh_TW"
 
 USER_LANGUAGES      = (
     (ENGLISH, u'English'),
@@ -27,7 +28,7 @@ class EntrakUser(AbstractUser):
     role_level = models.PositiveSmallIntegerField(max_length=20, choices=USER_ROLE_CHOICES, default=USER_ROLE_VIEWER_LEVEL)
     label = models.CharField(max_length=300, blank=True)
     department = models.CharField(max_length=100, blank=True)
-    language = models.PositiveSmallIntegerField(choices=USER_LANGUAGES, default=ENGLISH)
+    language = models.CharField(max_length=10, choices=USER_LANGUAGES, default=ENGLISH)
     is_email_verified = models.BooleanField(default=False)
     is_personal_account = models.BooleanField(default=False)
     salt = models.CharField(max_length=32, blank=True)
@@ -41,17 +42,20 @@ class EntrakUser(AbstractUser):
     @property
     def activation_url(self):
         utc_time_now = datetime.now()
-        return "https://en-trak.com/users/1/activate?uid=%s&ucode=%s"%(base64.b64encode(str(self.id)), base64.b64encode(str(utc_time_now - datetime(1970,1,1)).total_seconds()))
+        utc_timestamp = (utc_time_now - datetime(1970,1,1)).total_seconds()
+        encrypter = EntrakEncrypter(self.salt)
+        return "https://en-trak.com/users/1/activate?uid=%s&ucode=%s"%(encrypter.encode(str(self.id)), encrypter.encode(str(utc_timestamp)))
 
 
     def validate_activation_url(self, uid, ucode):
 
         try:
-            user_id = base64.b64decode(uid)
-            utc_timestamp = base64.b64decode(ucode)
-            utc_dt = datetime.fromtimestamp(utc_timestamp)
+            encrypter = EntrakEncrypter(self.salt)
+            user_id = encrypter.decode(uid)
+            utc_timestamp = encrypter.decode(ucode)
+            utc_dt = datetime.fromtimestamp(float(utc_timestamp))
 
-            return (user_id == u.id and (datetime.now() - utc_dt).days < 2)
+            return (user_id == str(self.id) and (datetime.now() - utc_dt).days < 2)
 
         except TypeError as e:
             return False
