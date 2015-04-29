7470e248280e4d55f8122775f448516bf7dece13
Yeung Leung <hotdogxz@gmail.com>
2015-04-29 17:35:34 +0800

    Fixed report summary weekly data calculation

--- companies/report_views.py
+++ companies/report_views.py
@@ -226,11 +226,15 @@ def summary_ajax(request, system_code):
     source_ids = [str(source.id) for source in sources]
     all_holidays = current_system.get_all_holidays()
 
+    # end_dt is at time 00:00:00 which excluded the last day
+    # add 1 more day to end_dt for actual calculation
+    end_dt = end_dt + relativedelta(days=1)
     day_source_readings = SourceManager.get_readings_with_target_class(source_ids, SourceReadingDay, start_dt, end_dt)
 
 
     compare_type = request.GET.get('compare_type')
     total_cost = get_total_cost(source_ids, start_dt, end_dt, compare_type)
+    print(total_cost)
 
     if compare_type == 'month':
         last_start_dt = start_dt - relativedelta(months=1)
