69e518e2f7031d04a3de92424a7656dd71776920
Eric Cheung <eric.cheung@en-trak.com>
2015-05-05 15:34:03 +0800

    Remove frequency name property

--- schedulers/models.py
+++ schedulers/models.py
@@ -9,10 +9,6 @@ class AutoSendReportSchedular(models.Model):
     last_execute_time = models.DateTimeField(auto_now_add=True)
     frequency = models.PositiveIntegerField()
 
-    @property
-    def frequency_name(self):
-        return FREQUENCIES.get(self.frequency)
-
 
 class AutoSendReportReceiver(models.Model):
     scheduler = models.ForeignKey('AutoSendReportSchedular', related_name="receivers")
