07f23a2a879e2841e32e1bd7efc0803eff506356
Eric Cheung <eric.cheung@en-trak.com>
2015-05-06 15:49:40 +0800

    Add the UrlToken model

--- token_url/models.py
+++ token_url/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class UrlToken(models.Model):
+    token_key = models.CharField(max_length=40)
+    url = models.URLField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    expiration_days = models.PositiveIntegerField()
