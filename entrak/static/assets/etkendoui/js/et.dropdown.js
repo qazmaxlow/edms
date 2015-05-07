7f6a25791c9f51549e986cb3fd62ae35d8975941
rickyhung <ricky.hung@en-trak.com>
2015-05-07 12:07:39 +0800

    extract dropdown widget

---
+++ entrak/static/assets/etkendoui/js/et.dropdown.js
@@ -0,0 +1,160 @@
+var EtDropDown = function(elm, data){
+    var that = this,
+    body = document.body,
+    keys = kendo.keys,
+    support = kendo.support,
+    ARIA_HIDDEN = "aria-hidden",
+    EMPTYITEMLIST = '<div class="k-popup-content"><div class="k-itemList"></div></div><div class="k-editPanel"><input></input><button class="add-btn">Add</button></div>',
+    SPAN = "<SPAN/>",
+    ns = ".kendoEtDropDown",
+    MOUSEDOWN = "mousedown" + ns,
+    KEYDOWN = "keydown" + ns,
+    CLICK = "click" + ns;
+    
+    that.wrapper = '<SPAN unselectable="on" class="k-widget k-dropdown k-header"><SPAN unselectable="on" class="k-dropdown-wrap k-state-default">'
+                + '<SPAN unselectable="on" class="k-input"></SPAN>' 
+                + '<SPAN unselectable="on" class="k-select"><SPAN unselectable="on" class="k-icon k-i-arrow-s"></SPAN></SPAN>'
+                + '</SPAN></SPAN>';
+
+    that.wrapper = $(that.wrapper);
+    that.wrapper.appendTo(elm);
+
+    that.options = {
+        readOnly: false,
+        itemDisplayName: "e-mails"
+    };
+
+    that.value(data);
+
+    that.container = $("<DIV/>").attr(ARIA_HIDDEN, "true").addClass("k-et-dropdown-container").appendTo(body);
+    var opt = {};
+    opt.animation = {};
+    opt.name = "Popup";
+    opt.isRtl = kendo.support.isRtl(opt.anchor);
+    opt.anchor = that.wrapper;
+
+    that.popup = new kendo.ui.Popup(that.container, opt);
+
+    var div = $("<DIV/>").attr("id", kendo.guid()).appendTo(that.popup.element);//.on(MOUSEDOWN, preventDefault);
+    div.html(EMPTYITEMLIST);
+
+    function addAction(){
+        if (that.addItem(that.container.find("INPUT").val())){
+            that.container.find("INPUT").val("");
+            that.updatePopup();
+        }        
+    }
+
+    that.container.on(KEYDOWN, function(e){
+        if (e.keyCode == keys.ESC) {
+            that.popup.close();
+            return;
+        } else if (e.keyCode == keys.ENTER) {
+            addAction();
+            return;
+        }
+    });
+
+    that.container.find(".add-btn").on(CLICK, addAction);
+
+    that.container.find(".k-itemList").on(CLICK, function(e){
+        var target = $(e.target);
+        if (target.hasClass("k-select") || target.hasClass("k-icon")){
+            var item = target.closest("DIV").find(".k-item").text();
+            that.removeItem(item);
+            that.updatePopup();
+        }
+    });
+    
+    that.wrapper.on(MOUSEDOWN, function(){
+        if (that.popup.visible()){
+            that.close();
+        } else {
+            that.open();    
+        }
+    });
+
+};
+
+EtDropDown.prototype = {
+    ITEM: '<DIV class="k-itemRow"><SPAN class="k-item"></SPAN><SPAN class="k-select"><SPAN class="k-icon k-delete"></SPAN></SPAN></DIV>',
+
+    value: function(data){
+        if (data == null){
+            return this.items;
+        } else if ($.isArray(data)){
+            this.items = data;
+        } else {
+            this.items = [data];
+        }
+    },
+
+    addItem: function(item){
+        if (item && item.trim()){
+            item = item.trim();
+            if (this.items.indexOf(item) == -1){
+                this.items.push(item);
+                this.updateTextbox();
+                return true;
+            }
+        }
+        return false;
+    },
+
+    removeItem: function(item){
+        if (item){
+            var i = this.items.indexOf(item);
+            if (i != -1){
+                this.items.splice(i, 1);
+            }
+            this.updateTextbox();
+        }
+    },
+
+    updatePopup: function(){
+        var list = $(this.popup.element).find(".k-itemList");
+        list.html("");
+
+        for (var i=0; i<this.items.length; i++){
+            var item = $(this.ITEM);
+            item.find(".k-item").html(this.items[i]);
+            item.appendTo(list);
+        }
+    },
+
+    updateTextbox: function(){
+        if (this.items.length == 1){
+            this.wrapper.find(".k-input").text(this.items[0]);
+        } else {
+            this.wrapper.find(".k-input").text(this.items.length + " " + this.options.itemDisplayName);
+        }
+    },
+
+    setOptions: function(opt){
+        var that = this;
+        if (opt && typeof opt == "object"){
+            $.each(opt, function(key, val){
+                that.options[key] = val;
+            });
+        }
+        that.wrapper.attr("aria-readonly", that.options.readOnly);
+        that.updateTextbox();
+    },
+
+    open: function(){
+        if (this.wrapper.attr("aria-readonly") == "true")
+            return;
+
+        var input = this.container.find("INPUT");
+        this.updatePopup();
+        input.val("");
+        this.popup.open();
+        setTimeout(function(){
+            input.focus();
+        }, 200);
+    },
+
+    close: function(){
+        this.popup.close();
+    }
+}
