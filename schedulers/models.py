31d90f882476d6f40844c822295e3996abb06d58
Eric Cheung <eric.cheung@en-trak.com>
2015-05-11 16:38:03 +0800

    Add created by in schedular

--- schedulers/models.py
+++ schedulers/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from constants.schedulers import FREQUENCIES
@@ -5,6 +6,7 @@ from constants.schedulers import FREQUENCIES
 
 class AutoSendReportSchedular(models.Model):
     system = models.ForeignKey('system.System')
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_time = models.DateTimeField(auto_now_add=True)
     last_execute_time = models.DateTimeField(auto_now_add=True)
     frequency = models.PositiveIntegerField()
