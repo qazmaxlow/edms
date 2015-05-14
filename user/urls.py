76cd1d40a6d80b59146dbcfd1d32ebac51aa5dcd
Yeung Leung <hotdogxz@gmail.com>
2015-05-14 18:52:33 +0800

    Added back missing user url

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     url(r'^activate$', views.activate_account, name='users.activate_account'),
     url(r'^update$', views.update_account, name='users.update_account'),
     url(r'^send_invitation_email$', views.send_invitation_email, name='users.send_invitation_email'),
+    url(r'^destroy$', views.DeleteUserView, name='users.disable_account'),
 )
