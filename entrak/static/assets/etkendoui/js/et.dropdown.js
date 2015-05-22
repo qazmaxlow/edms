fb19efd4ccf0c4048aa3500903d17acc5dc9c8f5
rickyhung <ricky.hung@en-trak.com>
2015-05-22 12:53:48 +0800

    auto report translation

--- entrak/static/assets/etkendoui/js/et.dropdown.js
+++ entrak/static/assets/etkendoui/js/et.dropdown.js
@@ -29,7 +29,7 @@ var EtDropDown = function(elm, data){
         isEmail: true,
         validationLabel: {
             duplicateEmail: "Duplicate Email",
-            invalidEmail: "Invalid Email."
+            invalidEmail: "Invalid Email"
         }
 
     };
