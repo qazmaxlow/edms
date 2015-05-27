a846b9e083e85e1bdfe72636476dd89ae6f847b2
Eric Cheung <eric.cheung@en-trak.com>
2015-05-27 11:50:24 +0800

    Get this year unit rates

--- companies/views/saving_views.py
+++ companies/views/saving_views.py
@@ -1,10 +1,32 @@
+import datetime
+import pytz
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from system.models import System
+
 
 class savingSoFarThisYear(APIView):
     def get(self, request, *args, **kwargs):
+        syscode = self.kwargs['system_code']
+        sys = System.objects.get(code=syscode)
+
+        # get this year unit rates
+        today = datetime.datetime.now(pytz.utc)
+        hktz = pytz.timezone("Asia/Hong_Kong")
+
+        this_year_first_date = datetime.datetime(
+            today.year,
+            1, 1
+        )
+        this_year_first_date = hktz.localize(this_year_first_date)
+
+        unitrates = sys.get_money_unitrates(start_from=this_year_first_date)
+
+        # if no unit rate for this year, use the latest one
+
         info = {}
         response = Response(info, status=status.HTTP_200_OK)
         return response
