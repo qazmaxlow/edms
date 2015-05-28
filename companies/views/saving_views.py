06144a6179f3732559363cd39ff119660df0db8d
Eric Cheung <eric.cheung@en-trak.com>
2015-05-28 10:54:57 +0800

    Reafactoring

--- companies/views/saving_views.py
+++ companies/views/saving_views.py
@@ -11,7 +11,56 @@ from rest_framework.views import APIView
 from system.models import System
 
 
+def get_saving(system, start_date, end_date, unitrates):
+    # if no unit rate for this year, use the latest one
+
+    # start_dt = this_year_first_date
+    # end_dt = timezone.now() + datetime.timedelta(days=1)
+
+    _ranges = []
+    # _st = this_year_first_date
+    _unitrate = unitrates[0]
+
+    if _unitrate.effective_date > start_date:
+        # get the unit rate for this year
+        unitrate = system.get_unit_rate(start_date)
+        date_ranges.append({'from': start_date,
+              'to': unitrate.effective_date,
+              'unitrate': unitrate}
+        )
+        _unitrate = unitrate
+
+    for unitrate in unitrates[1:]:
+        # _to = unitrate.effective_date
+        r = {'from': _unitrate.effective_date,
+              'to': unitrate.effective_date,
+              'unitrate': _unitrate}
+        _unitrate = unitrate
+        _ranges.append(r)
+
+    if _unitrate.effective_date < end_date:
+        _ranges.append({
+            'from': _unitrate.effective_date,
+            'to': end_date,
+            'unitrate': _unitrate
+        })
+
+    total_cost_changed = 0
+    for r in _ranges:
+        from_dt = r['from']
+        to_dt = r['to']
+        unitrate = r['unitrate']
+
+        current_kwh = system.get_total_kwh(from_dt, to_dt)
+        last_kwh = system.get_total_kwh(from_dt - relativedelta(years=1), to_dt - relativedelta(years=1))
+        cost_change = (current_kwh - last_kwh) * unitrate.rate
+        total_cost_changed += cost_change
+
+    return total_cost_changed
+
+
 class savingSoFarThisYear(APIView):
+
     def get(self, request, *args, **kwargs):
         syscode = self.kwargs['system_code']
         sys = System.objects.get(code=syscode)
@@ -27,50 +76,18 @@ class savingSoFarThisYear(APIView):
 
         unitrates = sys.get_unitrates(start_from=this_year_first_date)
 
+
         # if no unit rate for this year, use the latest one
 
         start_dt = this_year_first_date
         end_dt = timezone.now() + datetime.timedelta(days=1)
+        cost_changed = get_saving(
+            sys,
+            this_year_first_date,
+            timezone.now() + datetime.timedelta(days=1),
+            unitrates
+        )
 
-        _ranges = []
-        _st = this_year_first_date
-        _unitrate = unitrates[0]
-
-        if _unitrate.effective_date > this_year_first_date:
-            # get the unit rate for this year
-            unitrate = sys.get_unit_rate(this_year_first_date)
-            date_ranges.append({'from': this_year_first_date,
-                  'to': unitrate.effective_date,
-                  'unitrate': unitrate}
-            )
-            _unitrate = unitrate
-
-        for unitrate in unitrates[1:]:
-            # _to = unitrate.effective_date
-            r = {'from': _unitrate.effective_date,
-                  'to': unitrate.effective_date,
-                  'unitrate': _unitrate}
-            _unitrate = unitrate
-            _ranges.append(r)
-
-        if _unitrate.effective_date < end_dt:
-            _ranges.append({
-                'from': _unitrate.effective_date,
-                'to': end_dt,
-                'unitrate': _unitrate
-            })
-
-        total_cost_changed = 0
-        for r in _ranges:
-            from_dt = r['from']
-            to_dt = r['to']
-            unitrate = r['unitrate']
-
-            current_kwh = sys.get_total_kwh(from_dt, to_dt)
-            last_kwh = sys.get_total_kwh(from_dt - relativedelta(years=1), to_dt - relativedelta(years=1))
-            cost_change = (current_kwh - last_kwh) * unitrate.rate
-            total_cost_changed += cost_change
-
-        info = {'totalCostChanged': total_cost_changed}
+        info = {'totalCostChanged': cost_changed}
         response = Response(info, status=status.HTTP_200_OK)
         return response
