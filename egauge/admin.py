c1fbcb17ccfe13eac3e4ee9929c5c421c3663002
Eric Cheung <eric.cheung@en-trak.com>
2015-06-25 18:54:28 +0800

    Add admin page for status monitor

--- egauge/admin.py
+++ egauge/admin.py
@@ -1,3 +1,17 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import StatusMonitor
+
+
+class StatusMonitorForm(forms.ModelForm):
+
+    class Meta:
+        model = StatusMonitor
+
+
+class StatusMonitorAdmin(admin.ModelAdmin):
+    form = StatusMonitorForm
+
+
+admin.site.register(StatusMonitor, StatusMonitorAdmin)
