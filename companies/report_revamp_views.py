2e2ea4fb8b56b227d9026b20a7184c7e86c35416
Yeung Leung <hotdogxz@gmail.com>
2015-07-28 15:51:41 +0800

    Fixed report revamp bug when some sub-system contains empty data

--- companies/report_revamp_views.py
+++ companies/report_revamp_views.py
@@ -902,26 +902,23 @@ def _popup_report_view(request, system_code, year=None, month=None, report_type=
             highest_weekday_usage = generate_weekday_weekend_overnight_details(system, highest_weekday_date, highest_weekday_date + relativedelta(days=1), m["global_interval_delta"], m["global_sub_systems"], TYPE_COLORS, 'weekday')
         else:
             highest_weekday_usage = {'details': []}
-            highest_date_weekday_system = None
 
         if highest_weekend_date:
             highest_weekend_usage = generate_weekday_weekend_overnight_details(system, highest_weekend_date, highest_weekend_date + relativedelta(days=1), m["global_interval_delta"], m["global_sub_systems"], TYPE_COLORS, 'weekend')
         else:
             highest_weekend_usage = {'details': []}
-            highest_date_weekend_system = None
 
         if highest_overnight_date:
             highest_overnight_usage = generate_weekday_weekend_overnight_details(system, highest_overnight_date, highest_overnight_date + relativedelta(days=1), m["global_interval_delta"], m["global_sub_systems"], TYPE_COLORS, 'overnight')
         else:
             highest_overnight_usage = {'details': []}
-            highest_date_overnight_system = None
 
         weekday_diff = 0
-        weekday_diff_system = None
+        highest_date_weekday_system = None
         weekend_diff = 0
-        weekend_diff_system = None
+        highest_date_weekend_system = None
         overnight_diff = 0
-        overnight_diff_system = None
+        highest_date_overnight_system = None
 
         for avg in m['s3_weekday']['details']:
             for highest in highest_weekday_usage['details']:
