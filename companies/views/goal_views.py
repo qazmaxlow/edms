c4d17c937eb56f1040dbb5dce152487765835d9d
Eric Cheung <eric.cheung@en-trak.com>
2015-08-05 13:03:27 +0800

    Reload the grid if create for 12 months records

--- companies/views/goal_views.py
+++ companies/views/goal_views.py
@@ -41,6 +41,43 @@ class GoalSettingsView(generics.ListAPIView):
 class CreateGoalSettingView(generics.CreateAPIView):
     serializer_class = GoalSerializer
 
+    def create(self, request, *args, **kwargs):
+        goal_type = request.data['goal_type']
+
+        if goal_type == '1':
+            import datetime
+            import pytz
+            from dateutil.relativedelta import relativedelta
+
+            now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+            now = now.astimezone(request.user.system.time_zone)
+
+            this_month = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+            next_month = this_month
+
+            response_data = []
+            for i in range(12):
+                next_month += relativedelta(months=1)
+
+                request_data = dict(request.data.items())
+                request_data['goal_type'] = '2'
+                request_data['validated_date'] = next_month
+
+                serializer = self.get_serializer(data=request_data)
+                serializer.is_valid(raise_exception=True)
+                self.perform_create(serializer)
+                headers = self.get_success_headers(serializer.data)
+                response_data.append(serializer.data)
+
+            return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class UpdateGoalSettingView(generics.UpdateAPIView):
     serializer_class = UpdateGoalSerializer
