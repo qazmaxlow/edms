3bfa4e36c85e80dd33100c62df4d93ddbbc2918a
Eric Cheung <eric.cheung@en-trak.com>
2015-08-06 16:22:58 +0800

    Set login required of system goal settings view

--- system/views.py
+++ system/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
 
 from system.models import System
+from utils.auth import permission_required
 
 
 class SystemGoalSettingsView(TemplateView):
     template_name="systems/goals/settings.html"
+
+    @method_decorator(permission_required())
+    def dispatch(self, request, *args, **kwargs):
+        return super(SystemGoalSettingsView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(SystemGoalSettingsView, self).get_context_data(**kwargs)
